201e38d44404cc2ad81cb1c18ea53cc4c1c12415
KUAN-JUNG HUANG <alion13579@gmail.com>
2019-09-28 12:14:51 -0700

    add log check

--- Jupiter/src/db/DBConnectionFactory.java
+++ Jupiter/src/db/DBConnectionFactory.java
@@ -1,17 +1,19 @@
 package db;
 import db.mysql.MySQLConnection;
+import db.mongodb.MongoDBConnection;
 
 public class DBConnectionFactory {
 	// This should change based on the pipeline.
-	private static final String DEFAULT_DB = "mysql";
+	private static final String DEFAULT_DB = "mongodb";
 	
 	public static DBConnection getConnection(String db) {
 		switch (db) {
 		case "mysql":
 			return new MySQLConnection();
 		case "mongodb":
-			//return MongoDBConnection.getInstance();
-			return null;
+			return new MongoDBConnection();
+
+			//return null;
 		// You may try other dbs and add them here.
 		default:
 			throw new IllegalArgumentException("Invalid db " + db);
